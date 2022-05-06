bf90301b213b647a877dae402856cf505798de0b
andresguti19fc <chavogutierrez19@gmail.com>
2022-05-06 20:15:29 -0300

    mejorando alerts

--- js/carrito.js
+++ js/carrito.js
@@ -5,20 +5,19 @@ let data,
   subtotal = 0,
   totalPagar = 0,
   url = "./base.json",
+  carritoAdicta = document.getElementById(`carritoAdicta`),
+  btnAgregarArticulo = document.getElementById("btnFormAgregar"),
+  pagarTodo = document.getElementById("pagarTodo"),
+  mostrarPago = document.getElementById("mostrarPago"),
+  contadorCarrito = document.getElementById("contadorCarrito"),
+  contadorCarritoPagarTodo = document.getElementById("contadorCarritoPagarTodo"),
   BBDD = [];
 /* ++++++++++++++++++++++++++++ */
 BBDD = JSON.parse(localStorage.getItem("BASEDEDATOS"));
-let carritoAdicta = document.getElementById(`carritoAdicta`);
 carritoAdicta.addEventListener("click", renderizarCarrito);
-let btnAgregarArticulo = document.getElementById("btnFormAgregar");
 btnAgregarArticulo.addEventListener("click", agregar);
-let pagarTodo = document.getElementById("pagarTodo");
 pagarTodo.addEventListener("click", pagarTodoCarrito);
-let mostrarPago = document.getElementById("mostrarPago");
-let contadorCarrito = document.getElementById("contadorCarrito");
-let contadorCarritoPagarTodo = document.getElementById(
-  "contadorCarritoPagarTodo"
-);
+
 async function renderizar() {
   const res = await fetch(url);
   data = await res.json();
@@ -44,8 +43,10 @@ async function renderizar() {
         </div>
         `;
   }
+
   localStorage.setItem("BASEDEDATOS", JSON.stringify(data));
 }
+renderizar();
 
 function agregarAlCarrito(i) {
   let producto = BBDD[i];
@@ -53,18 +54,33 @@ function agregarAlCarrito(i) {
   for (let j = 0; j < carrito.length; j++) {
     if (carrito[j].id == producto.id) {
       existe = true;
+    
       carrito[j].cantidad++;
+      Swal.fire({
+        position: 'top-end',
+          icon: 'success',
+          title: 'agrego uno mas al carrito',
+          showConfirmButton: false,
+          timer: 500
+        })
+        
       break;
     }
   }
   if (!existe) {
     producto.cantidad = 1;
     carrito.push(producto);
+    Swal.fire({
+      position: 'top-end',
+        icon: 'success',
+        title: 'se agrego al carrito',
+        showConfirmButton: false,
+        timer: 500
+      })
   }
   carritoAdicta.innerHTML = "";
   renderizarCarrito();
 }
-console.log(carrito);
 function renderizarCarrito() {
   for (let i = 0; i < carrito.length; i++) {
     let { nombre, precio, cantidad, id } = carrito[i];
@@ -89,12 +105,44 @@ function renderizarCarrito() {
   }
 }
 function eliminarDelCarrito(i) {
-  //eliminar del carrito
-  index = carrito.findIndex((producto) => producto.id == carrito[i].id);
-  console.log(index);
-    carrito.splice(index, 1);
+  console.log("eliminar del carrito");
+  /* 
+    carrito.splice(index, 1); */
+  Swal.fire({
+    title: '¿Estas seguro?',
+    text: "no podras revertir los cambios!",
+    icon: 'warning',
+    showCancelButton: true,
+    confirmButtonColor: '#3085d6',
+    cancelButtonColor: '#d33',
+    confirmButtonText: 'Si, eliminar!'
+  }).then((result) => {
+    if (result.isConfirmed) {
+      console.log("confirmar eliminaciion");
     
-    carritoAdicta.innerHTML = "";
+      Swal.fire(
+        'Listo!',
+        'El producto fue.',
+        'success'
+      )
+      
+  
+    }
+    
+  })
+  let { id } = carrito[i];
+  carrito.splice(i, 1);
+  carritoAdicta.innerHTML = "";
+  contadorCarrito.innerHTML = carrito.length;
+  total2 = carrito.reduce(
+    (accum, producto) => accum + producto.precio * producto.cantidad,
+    0
+  );
+  subtotal = total2;
+  totalPagar = total2;
+  contadorCarritoPagarTodo.innerHTML = carrito.length;
+  
+  /* carritoAdicta.innerHTML = ""; */
 
 
 }
@@ -106,28 +154,6 @@ function comprarCarrito(i) {
   carrito.splice(i, 1);
   carritoAdicta.innerHTML = "";
 }
-
-/* function agregar(nombre, precio, imagen, id, cantidad) {
-    
-   nombre.preventDefault();
-   let imagenInput = document.getElementById("imagenFormAgregar").value;
-    let inputAgregarArticulo = document.getElementById("nombreFormAgregar").value;
-    let inputAgregarPrecio = document.getElementById("precioFormAgregar").value;
-   imagen = imagenInput; 
-   nombre=inputAgregarArticulo;
-   precio=inputAgregarPrecio;
-cantidad=0;
-id= BBDD.length + 1;
-    let nuevoArticulo = JSON.parse(localStorage.getItem("BASEDEDATOS"));
-    nuevoArticulo.push(new Basededatos(imagen, nombre, precio, id, cantidad));
-    BBDD = [...BBDD, nuevoArticulo];
-    localStorage.setItem("BASEDEDATOS", JSON.stringify(BBDD));
-
-  
-    tiendaAdicta.innerHTML = "";
-  renderizar();
-}  */
-
 function agregar(e) {
   e.preventDefault();
   let nombre = document.getElementById("nombreFormAgregar").value;
@@ -145,9 +171,35 @@ function agregar(e) {
   };
   BBDD = [...BBDD, nuevoArticulo];
   localStorage.setItem("BASEDEDATOS", JSON.stringify(BBDD));
-}
-renderizar();
+  BBDD = JSON.parse(localStorage.getItem("BASEDEDATOS"));
+  BBDD.forEach((producto) => {
+    
+    tiendaAdicta.innerHTML = "";
+    tiendaAdicta.innerHTML += `
+    <div class="col-md-4 my-5">
+            <div class="card h-100">
+            <img class="card-img-top img-fluid" src="images/${producto.imagen}" alt="..." />
+                <div class="card-body p-4">
+                <div class="text-center">
+                    <h5 class="card-title ">${producto.nombre}</h5>
+                    <p class="card-text">
+                        <strong>precio:</strong> ${producto.precio}$
+                    </p>
+                </div>
+                <div class="card-footer p-4 pt-0 border-top-0 bg-transparent mt-5">
+        <div class="text-center"><button class="btn btn-outline-dark mt-auto mensajeComprar" onclick="agregarAlCarrito(${producto.length + 1})" id="${producto.id}" >Agregar al carrito</button></div>
+        </div>
+        </div>
+        </div>
+        </div>
+    `;
+    console.log(producto);
+    console.log(BBDD);
+    }
 
+    );
+    renderizar();
+}
 
 function pagarTodoCarrito() {
   const DateTime = luxon.DateTime;
@@ -156,26 +208,37 @@ function pagarTodoCarrito() {
   const minuto = DateTime.local().minute;
   const segundo = DateTime.local().second;
 
-  swal.fire("Gracias por comprar", `total a pagar: ${total2}$`, "success");
-  carrito.splice(0, carrito.length);
-  mostrarPago.innerHTML = `
-  <div class="container px-4 px-lg-5 mt-5">
-  <div class="row gx-4 gx-lg-5 align-itemns-center justify-content-center">
-  <div class="col-md-12">
-  <div class="alert alert-dark" role="alert">
-  <h4 class="alert-heading">Gracias por comprar</h4>
-  <p>total a pagar: ${total2}$</p>
-  <p>fecha: ${date.toFormat("dd/MM/yyyy")}</p>
-  <p>hora: ${hora}:${minuto}:${segundo}</p>
-  <div>
-  <h5>articulos en el carrito:</h5>  
-  </div>
-  </div>
-  </div>
-  </div>
+  setTimeout(() => {
+    Swal.fire({
+      title: `
+      <div class="container px-4 px-lg-5 mt-5">
+      <div class="row gx-4 gx-lg-5 align-itemns-center justify-content-center">
+      <div class="col-md-12">
+      <div class="alert alert-light" role="alert">
+      <h4 class="alert-heading">Gracias por comprar</h4>
+      <p>total a pagar: ${total2}$</p>
+      <p>fecha: ${date.toFormat("dd/MM/yyyy")}</p>
+      <p>hora: ${hora}:${minuto}:${segundo}</p>  
+      </div>
+      </div>
+      </div>
+      </div>
+    
+      `,
+      width: 800,
+      padding: '1em',
+      color: '#716add',
+      showConfirmButton: false,
+            timer: 2000
+    })
+    
+      
+    carrito.splice(0, carrito.length);
+  }, 500);
 
-  `;
-  carritoAdicta.innerHTML = "";
+  carritoAdicta.innerHTML = ""; 
+  contadorCarrito = 0;
+  total2 = 0;
 }
 /*** datos de la compra ***/
 
@@ -217,3 +280,4 @@ document.getElementById("carritoCompras").addEventListener("click", (e) => {
     return abrirVentana("carritoDeCompras");
     }
     );
+
